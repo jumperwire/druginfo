46514ea936a504376ead8146cf6573aa60b9b05a
jumperwire <hendrasutanto@gmail.com>
2019-09-02 16:54:35 +0800

    Update productpage.py

--- src/productpage/productpage.py
+++ src/productpage/productpage.py
@@ -279,7 +279,7 @@ def getProducts():
         {
             'id': 0,
             'title': 'Augmentin',
-            'descriptionHtml': 'Augmentin (amoxicillin and clavulanate) is a combination penicillin-type antibiotic used to treat a wide variety of bacterial infections. It works by stopping the growth of bacteria.'
+            'descriptionHtml': 'Augmentin is a combination penicillin type antibiotic used to treat a wide variety of bacterial infections. It works by stopping the growth of bacteria.'
         }
     ]
 
